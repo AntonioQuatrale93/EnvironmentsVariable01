cb46fd29c5685deb33fb374fa54c4cc50ff70449
AntonioQuatrale93 <103583055+AntonioQuatrale93@users.noreply.github.com>
2022-08-16 15:30:43 +0200

    Add files via upload

---
+++ EnvironmentVariable01/src/test/java/com/example/EnvironmentVariable01/EnvironmentVariable01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnvironmentVariable01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentVariable01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
